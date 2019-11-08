9b7a5e27e0f30d532490be30f4a6c1eb12aae591
skybe <alabracadabrador@gmail.com>
2019-11-08 16:44:15 +0300

    adding the keyboard with buttons

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -4,21 +4,23 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
     public static void main(String[] args) {
 
-//        System.getProperties().put("proxySet", "true");
-//        System.getProperties().put("socksProxyHost", "127.0.0.1");
-//        System.getProperties().put("socksProxyPort", "9150");
-
-        // инициализируем API
+        // initializing the API
         ApiContextInitializer.init();
-        // создаем объект Telegram API
+        // creating the Telegram API object
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
 
-        // далее нужно зарегистрировать бота
+        // then register the bot
         try {
             telegramBotsApi.registerBot(new Bot());
         } catch (TelegramApiException e) {
@@ -27,8 +29,6 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-    // Метод для приема сообщений. Он используется для приема обновлений через long pool
-    // (существует несколько вариантов обновлений - web hooks или long pool)
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
 
@@ -66,9 +66,43 @@ public class Bot extends TelegramLongPollingBot {
         sendMessage.setText(text);
 
         try {
+            // invoking the setButtons method to attach the keyboard
+            setButtons(sendMessage);
+            // sends the pre-defined text to the user
             sendApiMethod(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+    }
+
+    // create keyboard schema for the user
+    public void setButtons(SendMessage sMessage) {
+        // initializing the keyboard
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+
+        // setting the markup for the keyboard with the messages
+        sMessage.setReplyMarkup(replyKeyboardMarkup);
+        // make the bot reveal the keyboard to the specified users
+        replyKeyboardMarkup.setSelective(true);
+        // the markup of the keyboard relates to the quantity of function buttons
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        // provides the ability to hide the keyboard
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+
+        // creating the rows of buttons
+        List<KeyboardRow> listOfButtonRows = new ArrayList<>();
+        // create the first row of buttons
+        KeyboardRow firstRowOfButtons = new KeyboardRow();
+        // adding a button to the first row
+        firstRowOfButtons.add(new KeyboardButton("/help"));
+        firstRowOfButtons.add(new KeyboardButton("/settings"));
+
+        // adding the first row to the keyboard rows list
+        listOfButtonRows.add(firstRowOfButtons);
+
+        // attach created keyboard to the markup
+        replyKeyboardMarkup.setKeyboard(listOfButtonRows);
+
     }
 }
